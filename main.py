b6eab97f70679a621e4dd5cb7e0924f33416e9b8
TeoDar <nikyl0777@gmail.com>
2023-06-23 20:16:07 +0300

    123

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from multiprocessing import Process, Manager
 
 battle_end = f"./samples/battle_end.png"
 point = "./samples/point.png"
-purp_point = "./samples/purp_point.png" #255+-5 76+-30 255+-10
+purp_point = "./samples/purp_point.png"  # 255+-5 76+-30 255+-10
 blue_point = "./samples/blue_point.png"
 last_proc: Process
 
@@ -14,8 +14,12 @@ def clicking(loc) -> None:
     print(f"        Нашёл: {loc}")
     moveTo(loc)
     mouseDown()
-    sleep(2)
+    sleep(1.5)
     return loc
+
+
+def search_purp():
+    pass
 
 def filtering(point_location):
     lasc_loc = None
@@ -27,15 +31,12 @@ def filtering(point_location):
                 print(f"####Пропуск {loc}")
                 continue
         clicking(loc)
-        Amount -= 1
         lasc_loc = loc
-                
 
-def check_battle_end() ->(Point | None):
+
+def check_battle_end() -> Point | None:
     searching_start = locateCenterOnScreen(battle_end, confidence=0.6, grayscale=True)
     return searching_start if searching_start else None
-
-
 
 
 def searching(SEARCHING_ACTIVE: Manager):
@@ -49,7 +50,7 @@ def searching(SEARCHING_ACTIVE: Manager):
                 print(f"\nЦенностей не найдено\n")
                 break
             else:
-                filtering()
+                filtering(point_locations)
             print("    Добыча завершена\n")
             sleep(5)
     print('Для запуска/остановки поиска нажмите "F2"\nДля завершения нажмине "F3"')
